be953a8b2b70df3415bd4f44c406c86c3f59a47c
Maksim Koshman <maksimkoshman090@gmail.com>
2021-02-18 17:21:23 +0300

    Добавил 3 новых страницы: news, bonus и prognoz

--- app/js/app.js
+++ app/js/app.js
@@ -11,26 +11,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const searchHints = document.querySelector(".search__hints");
   const openSearch = document.querySelector("#openSearch");
   const headerIntroCenter = document.querySelector(".header__intro__center");
+  const socialBtn = document.querySelector(".social__btn");
+  const social = document.querySelector(".social");
 
   // Закрыть все вкладки
-  function closeAll(items) {
-    items.forEach((item) => {
-      item.classList.remove("header__nav__item--active");
-    });
-  }
-
   mask.addEventListener("click", () => {
     body.classList.remove("fixed");
     headerIntroCenter.classList.remove("header__intro__center--active");
     burgerBtn.classList.remove("header__burger--active");
     navInner.classList.remove("header__intro__nav--active");
-    closeAll(navItems);
+
+    if (social) {
+      social.classList.remove("social--active");
+      socialBtn.classList.remove("social__btn--active");
+    }
+
     mask.classList.remove("mask--active");
   });
 
   // Меню
   burgerBtn.addEventListener("click", () => {
-    closeAll(navItems);
     burgerBtn.classList.toggle("header__burger--active");
     navInner.classList.toggle("header__intro__nav--active");
 
@@ -87,6 +87,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
       });
     } else {
       headerNav.classList.remove("header__nav--scroll");
+      // mask.classList.remove("mask--active");
 
       navItems.forEach((navItem) => {
         navItem.removeEventListener("mouseover", maskOpen);
@@ -150,15 +151,163 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 
   // Slider
-  new Glider(document.querySelector(".experts__line"), {
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    draggable: true,
-    infinity: true,
-    dots: false,
-    arrows: {
-      prev: ".experts__btn--prev",
-      next: ".experts__btn--next",
-    },
+  if (document.querySelector(".experts__line")) {
+    new Glider(document.querySelector(".experts__line"), {
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      draggable: true,
+      dots: false,
+      arrows: {
+        prev: ".experts__btn--prev",
+        next: ".experts__btn--next",
+      },
+    });
+  }
+
+  // News
+  // Соцсети
+  if (socialBtn) {
+    socialBtn.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      social.classList.toggle("social--active");
+      socialBtn.classList.toggle("social__btn--active");
+
+      if (window.innerWidth < 1200) {
+        mask.classList.toggle("mask--active");
+      }
+    });
+  }
+
+  // Табы в фильтре
+  const filterLinesWrapper = document.querySelectorAll(
+    ".filter__line__wrapper"
+  );
+  filterLinesWrapper.forEach((filterLine) => {
+    filterLine.addEventListener("click", () => {
+      filterLine.parentNode.classList.toggle("filter__line--active");
+    });
   });
+
+  // Закрыть фильтр
+  const closeFilter = document.querySelector("#closeFilter");
+  const filterBox = document.querySelector(".whole__left");
+
+  if (closeFilter) {
+    closeFilter.addEventListener("click", () => {
+      filterBox.classList.remove("whole__left--bonus");
+      mask.classList.remove("mask--active");
+      body.classList.remove("fixed");
+    });
+  }
+
+  // Открыть фильтр
+  const openFilter = document.querySelector("#openFilter");
+  if (openFilter) {
+    openFilter.addEventListener("click", () => {
+      filterBox.classList.add("whole__left--bonus");
+      mask.classList.add("mask--active");
+      body.classList.add("fixed");
+    });
+  }
+
+  // Progonoz
+  if (document.querySelector("#prognozSlider")) {
+    let prognozSlider = new Glider(document.querySelector("#prognozSlider"), {
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: false,
+      arrows: {
+        prev: ".prognoz__btn--prev",
+        next: ".prognoz__btn--next",
+      },
+    });
+
+    if (window.innerWidth < 1199) {
+      prognozSlider.destroy();
+    }
+  }
+
+  // Переключение вкладок
+  const prognozPannelItems = document.querySelectorAll(
+    ".prognoz__pannel__item"
+  );
+  const prognozSeries = document.querySelectorAll(".prognoz__series");
+
+  function resetPrognoz() {
+    prognozPannelItems.forEach((prognozPannelItem) => {
+      prognozPannelItem.classList.remove("prognoz__pannel__item--current");
+    });
+
+    prognozSeries.forEach((prognozSerieslItem) => {
+      prognozSerieslItem.classList.remove("prognoz__series--active");
+    });
+  }
+
+  if (prognozPannelItems) {
+    prognozPannelItems.forEach((prognozPannelItem) => {
+      prognozPannelItem.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        resetPrognoz();
+        prognozPannelItem.classList.add("prognoz__pannel__item--current");
+        const currDataTab = prognozPannelItem.dataset.tab;
+        const currSeriesSection = document.getElementById(currDataTab);
+        currSeriesSection.classList.add("prognoz__series--active");
+      });
+    });
+  }
+
+  // Books
+  // Переключение вкладок
+  const booksPannelItems = document.querySelectorAll(".books__pannel__item");
+  const booksSeries = document.querySelectorAll(".books__body");
+
+  function resetbooks() {
+    booksPannelItems.forEach((booksPannelItem) => {
+      booksPannelItem.classList.remove("books__pannel__item--active");
+    });
+
+    booksSeries.forEach((booksSerieslItem) => {
+      booksSerieslItem.classList.remove("books__body--active");
+    });
+  }
+
+  if (booksPannelItems) {
+    booksPannelItems.forEach((booksPannelItem) => {
+      booksPannelItem.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        resetbooks();
+        booksPannelItem.classList.add("books__pannel__item--active");
+        const currDataTab = booksPannelItem.dataset.books;
+        const currSeriesSection = document.getElementById(currDataTab);
+        currSeriesSection.classList.add("books__body--active");
+      });
+    });
+  }
+
+  // Открыть карточки
+  const booksOpenBtns = document.querySelectorAll(".books__app__open");
+  if (booksOpenBtns) {
+    booksOpenBtns.forEach((booksOpenBtn) => {
+      booksOpenBtn.addEventListener("click", () => {
+        const currBookLine = findAncestor(booksOpenBtn, "books__app");
+        currBookLine.classList.toggle("books__app--active");
+      });
+    });
+  }
+
+  // Читать полностью
+  const readMoreList = document.querySelectorAll(".best__link__inner");
+  if (readMoreList) {
+    readMoreList.forEach((readMoreItem) => {
+      readMoreItem.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        const linkParent = readMoreItem.parentNode;
+        const currHiddenText = linkParent.querySelector(".hide");
+        currHiddenText.classList.remove("hide");
+
+        readMoreItem.remove();
+      });
+    });
+  }
 });
